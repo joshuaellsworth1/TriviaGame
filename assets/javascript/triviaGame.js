289dc72638fe786bd8cdbcad3a61b49cc9dac36f
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-07-07 16:15:01 -0400

    Revised HTML, CSS, and Javascript. Not showing in browser

--- assets/javascript/triviaGame.js
+++ assets/javascript/triviaGame.js
@@ -1,68 +1,90 @@
-var score = 0;
-var currentQuestion = 0;
-var questions = [
-    {
-        title: "What color is the sky?",
-        answers: ['yellow', 'blue', 'orange', 'green'],
-        correct: 1
-    },
-    {
-        title: "What color is grass?",
-        answers: ['yellow', 'blue', 'orange', 'green'],
-        correct: 3
-    },
-    {
-        title: "What color is the sun?",
-        answers: ['yellow', 'blue', 'orange', 'green'],
-        correct: 0
-    },
-    {
-        title: "What color is cheetos?",
-        answers: ['yellow', 'blue', 'orange', 'green'],
-        correct: 2
-    },
-]
+// $(document).ready(function() {
 
-$(document).ready(function () {
-    $('.start a').click(function (e) {
-        e.preventDefault();
-        $('.start').hide();
-        $('.quiz').show();
-        showQuestion();
-    });
-    $('.quiz ul').on('click', 'li', function () {
-        $('.selected').removeClass('selected');
-        $(this).addClass('selected');
-    });
-    $('.quiz a').click(function (e) {
-        e.preventDefault();
-        if ($('li.selected').length) {
-            var guess = $('li.selected').attr('id');
-            console.log(guess);
-        } else {
-            alert('Please selected an answer');
-        }
-    })
+//     function Question(text, choices, answer) {
+//     this.text = text;
+//     this.choices = choices;
+//     this.answer = answer;
+//     }
+//     console.log(Question);
 
-});
+//     Question.prototype.correctAnswer = function (choice) {
+//     return choice === this.answer;
 
-function startQuiz() {
+//     }
 
-}
+//     function Quiz(questions) {
+//     this.score = 0;
+//     this.questions = questions;
+//     this.questionIndex = 0;
+//     }
 
-function showQuestion() {
-    var question = questions[currentQuestion];
-    $('.quiz h2').text(question.title);
-    $('.quiz ul').html('');
-    for (var i = 0; i < question.answers.length; i++) {
-        $('.quiz ul').append("<li id = '"+i+"'>" + question.answers[i] + "</li>");
-    }
-};
+//     Quiz.prototype.getQuestionIndex = function () {
+//     return this.questions[this.questionIndex];
+//     }
 
-function checkAnswer() {
+//     Quiz.prototype.isEnded = function () {
+//     return this.questions.length === this.questionIndex;
+//     }
 
-}
+//     Quiz.prototype.guess = function (answer) {
+//     this.questionIndex++;
 
-function showSummary() {
+//     if (this.getQuestionIndex().correctAnswer(answer)) {
+//         this.score++;
+//     }
+//     this.question++;
+//     }
 
-}
+//     function populate() {
+//     if (quiz.isEnded()) {
+//         showScore();
+//     }
+//     else {
+//         //show question
+//         var element = document.getElementById("question");
+//         element.innerHTML = quiz.getQuestionIndex().text;
+
+//         //show choices
+//         var choices = quiz.getQuestionIndex().choices;
+//         for (var i = 0; i < choices.length; i++) {
+//             var element = document.getElementById("choice" + i);
+//             element.innerHTML = choices[i];
+//             guess("btn" + i, choices[i]);
+//         }
+//         showProgress();
+//     }
+//     }   ;
+
+//     function guess(id, guess) {
+//     var button = document.getElementById(id);
+//     $("button").onclick = function () {
+//         $("quiz").guess(guess);
+//         populate();
+//     }
+//     }
+
+//     function showProgress() {
+//     var currentQuestionNumber = quiz.questionIndex + 1;
+//     var element = document.getElementById("progress");
+//     element.innerHTML = "Question" + currentQuestionNumber + "of" + quiz.questions.length;
+//     }
+
+//     function showScore() {
+//     var gameOverHTML = "<h1>Result</h1>";
+//     gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "<h2>";
+//     var element = document.getElementById("quiz");
+//     element.innerHTML = gameOverHTML;
+//     }
+
+//     var questions = [
+//     new question("Which one is not an object oriented programming language?", ["Java", "C#", "C++", "C"], "C"),
+//     new question("Which language is used for styling web pages?", ["HTML", "JQuery", "CSS", "XML"], "CSS"),
+//     new question("There are ___ main components of object oriented programming.", ["1", "6", "2", "4"], "4"),
+//     new question("Which language is used for web apps?", ["PHP", "Python", "Javascript", "All"], "All")
+//     ];
+//     console.log(questions);
+//     var quiz = new Quiz(questions);
+
+//     populate();
+// });
+
