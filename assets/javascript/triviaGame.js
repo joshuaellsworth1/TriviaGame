1edcb42131adeaf1673cca5d417c2c5ce482ecdc
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-07-06 22:02:53 -0400

    Restructured HTML

--- assets/javascript/triviaGame.js
+++ assets/javascript/triviaGame.js
@@ -1,66 +1,42 @@
-var i;
-var counter = 60;
-var intevalId;
+$(document).ready(function () {
+    var counter = 60;
+    var intevalId;
+    var correct = 0;
 
-$("#stop").on("click", stop);
-$("#resume").on("click", run);
+    $("#stop").on("click", stop);
+    $("#resume").on("click", run);
 
-function run() {
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-  }
-
-  function decrement() {
-    counter--;
-    $("#show-counter").html("<h2>" + counter + "</h2>");
-    if (counter === 0) {
-    stop();
-    alert("Time's Up!");
+    function run() {
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
     }
-}
 
-function stop() {
-    clearInterval(intervalId);
-}
-run();
-
-var questions = [{
-    question1: "what is the capital of pennsylvania",
-    choice1: "philadelphia",
-    choice2: "harrisburg",
-    answer: "1"
-},
-{
-    question2: "where is canada located",
-    choice1: "above the united states",
-    choice2: "below the united states",
-    answer: "1"
-}
-]
-
-function progressRender() {
-    for (i = 0; i < questions.length; i++) {
-
+    function decrement() {
+        counter--;
+        $("#show-counter").html("<h1>" + counter + "</h1>");
+        if (counter === 0) {
+            stop();
+            alert("Time's Up!");
+        }
     }
+});
+
+var questions = [
+    [1, "What is the capital of Pennsylvania", "harrisburg"],
+    [2, "Where is Canada located?", "above the united states"],
+    [3, "Is pikachu a rat or a mouse?", "a mouse"]
+];
+
+var answer;
+var reponse;
+
+for (var i = 0; i < questions.length; i += 1) {
+    answer = prompt(questions[i]);
+    response = answer.toLowerCase();
 }
 
-function rightAnswer() {
-    $("#runningQuestion").alert(function () {
-    }
-}
-
-function wrongAnswer() {
-    $("#runningQuestion").alert(function () {
-    }
-}
-
-var questions = questions.length - 1;
-var runningQuestion = 0;
-
-function renderQuestion() {
-    var q = questions[runningQuestion];
-    choice1 = q.choice1;
-    choice2 = q.choice2;
-
-    runningQuestion++;
+if (response === questions[i][2]); {
+    document.write("correct!");
+} else {
+    document.write("wrong)");
 }
