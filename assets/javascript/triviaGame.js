9f68efa52c31c25d8c493ae72e1a0ff031c10b23
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-07-08 17:43:36 -0400

    Finished Javascript. Funcional, just needs new questions and revamped CSS

--- assets/javascript/triviaGame.js
+++ assets/javascript/triviaGame.js
@@ -1,16 +1,46 @@
+var quizContainer = $("#quiz");
+var resultsContainer = $("#results");
+var submitButton = $("#submit");
+var numCorrect = 0;
+var output = [];
+
+
+var myQuestions = [{
+    question: "What bear is best?",
+    answers: {
+        a: "Polar Bear",
+        b: "Black Bear",
+        c: "Care Bear"
+    },
+    correctAnswer: "b"
+},
+{
+    question: "What is the best site ever created?",
+    answers: {
+        a: "SitePoint",
+        b: "Simple Steps Code",
+        c: "Trick question; they're both the best"
+    },
+    correctAnswer: "c"
+},
+{
+    question: "Where is Waldo really?",
+    answers: {
+        a: "Antarctica",
+        b: "Exploring the Pacific Ocean",
+        c: "Sitting in a tree",
+        d: "Minding his own business, so stop asking"
+    },
+    correctAnswer: "d"
+}
+];
+
 (function () {
     function buildQuiz() {
-        // we'll need a place to store the HTML output
-        const output = [];
-
-        // for each question...
         myQuestions.forEach((currentQuestion, questionNumber) => {
-            // we'll want to store the list of answer choices
-            const answers = [];
+            var answers = [];
 
-            // and for each available answer...
             for (letter in currentQuestion.answers) {
-                // ...add an HTML radio button
                 answers.push(
                     `<label>
               <input type="radio" name="question${questionNumber}" value="${letter}">
@@ -20,7 +50,6 @@
                 );
             }
 
-            // add this question and its answers to the output
             output.push(
                 `<div class = "question" > ${currentQuestion.question} </div>
           <div class = "answers" > ${
@@ -29,20 +58,64 @@
             );
         });
 
-        // finally combine our output list into one string of HTML and put it on the page
         quizContainer.innerHTML = output.join("");
     }
 
     function showResults() {
-        // gather answer containers from our quiz
-        const answerContainers = quizContainer.querySelectorAll(".answers");
+        var answerContainers = quizContainer.querySelectorAll(".answers");
 
-        // keep track of user's answers
-        let numCorrect = 0;
-
-        // for each question...
         myQuestions.forEach((currentQuestion, questionNumber) => {
-            // find selected answer
-            const answerContainer = answerContainers[questionNumber];
-            const selector = `input[name=question${questionNumber}]:checked`;
-            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+            var answerContainer = answerContainers[questionNumber];
+            var selector = `input[name=question${questionNumber}]:checked`;
+            var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+            if (userAnswer === currentQuestion.correctAnswer) {
+                numCorrect++;
+
+                answerContainers[questionNumber].style.color = "lightgreen";
+            } else {
+                answerContainers[questionNumber].style.color = "red";
+            }
+        });
+
+        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
+
+    var quizContainer = document.getElementById("quiz");
+    var resultsContainer = document.getElementById("results");
+    var submitButton = document.getElementById("submit");
+    var myQuestions = [{
+        question: "What bear is best?",
+        answers: {
+            a: "Polar Bear",
+            b: "Black Bear",
+            c: "Care Bear"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What is the best site ever created?",
+        answers: {
+            a: "SitePoint",
+            b: "Simple Steps Code",
+            c: "Trick question; they're both the best"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Where is Waldo really?",
+        answers: {
+            a: "Antarctica",
+            b: "Exploring the Pacific Ocean",
+            c: "Sitting in a tree",
+            d: "Minding his own business, so stop asking"
+        },
+        correctAnswer: "d"
+    }
+    ];
+
+    buildQuiz();
+
+    submitButton.addEventListener("click", showResults);
+})();
