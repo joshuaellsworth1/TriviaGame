ab2fb331ea044621f49dbb292ec19cbf4f1dc31e
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-07-10 21:34:42 -0400

    creating if statements for the questions

--- assets/javascript/triviaGame.js
+++ assets/javascript/triviaGame.js
@@ -1,36 +1,45 @@
-var score = 0;
+var correct = 0;
 var currentQuestion = 0;
 
-var questions = [ {
-    q: "what is 2 + 2?"
+
+var questions = [{
+    q1: "what is 2 + 2?"
     a: ["1", "2", "3", "4"],
     correct: 3
-}, 
+},
 {
-    q: "what is 2 + 1?"
+    q2: "what is 2 + 1?"
     a: ["1", "2", "3", "4"],
     correct: 2
 },
 {
-    q: "what is 2 + 2?"
+    q3: "what is 2 + 2?"
     a: ["1", "2", "3", "4"],
     correct: 3
 },
 {
-    q: "what is 2 + 2?"
+    q4: "what is 2 + 2?"
     a: ["1", "2", "3", "4"],
     correct: 3
 }
 ]
 
-var question = $('questions');
-var answers = $('a');
-var correctAns = $('correct');
-var submit = $('submit');
-var answers = $('answers');
-var array = questions.length;
-
-function questionList (questionIndex) {
-    var q = questions[questionIndex];
-    
+if (q1 == "4") {
+    correct++;
 }
+if (q2 == "3") {
+    correct++;
+}
+
+// var question = $('questions');
+// var answers = $('a');
+// var correctAns = $('correct');
+// var submit = $('submit');
+// var answers = $('answers');
+// var array = questions.length;
+
+// var newQuestion = $("<div>");
+// for (var i = 0; i < questions.length; ++i) {
+//     questions = this.newQuestion[i];
+//     $('question').append('newQuestion')
+// }
