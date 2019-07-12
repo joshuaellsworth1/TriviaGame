c46789ca65637b0b4dd6be34eb78f149a8fba803
Josh Ellsworth <joshuaellsworth1@gmail.com>
2019-07-11 22:23:48 -0400

    functional game

--- assets/javascript/triviaGame.js
+++ assets/javascript/triviaGame.js
@@ -1,45 +1,82 @@
-var correct = 0;
-var currentQuestion = 0;
-
+$(document).ready(function() {
+var correctCounter = 0;
+var incorrectCounter = 0;
 
 var questions = [{
-    q1: "what is 2 + 2?"
+    question: "what is 2 + 2?",
     a: ["1", "2", "3", "4"],
-    correct: 3
+    correct: "4"
 },
 {
-    q2: "what is 2 + 1?"
-    a: ["1", "2", "3", "4"],
-    correct: 2
+    question: "Who is wooody's best friend?",
+    a: ["sam", "gator", "buzz", "raymond"],
+    correct: "buzz"
 },
 {
-    q3: "what is 2 + 2?"
+    question: "what is 2 + 0?",
     a: ["1", "2", "3", "4"],
-    correct: 3
+    correct: "2"
 },
 {
-    q4: "what is 2 + 2?"
-    a: ["1", "2", "3", "4"],
-    correct: 3
+    question: "what is 2 + 5?",
+    a: ["1", "2", "3", "7"],
+    correct: "7"
 }
 ]
 
-if (q1 == "4") {
-    correct++;
-}
-if (q2 == "3") {
-    correct++;
+// $("#Question-guess").text(currentQuestion);
+function displayQuestions() {
+    for (var i = 0; i < questions.length; i++){
+        $('#display').append("<p>" + questions[i].question + "</p>");
+        for (var j = 0; j < questions[i].a.length; j++) {
+            $("#display").append("<label><input type='radio' name='" + i + "' value='" + questions[i].a[j] + "'>" + questions[i].a[j] + "</label>");
+        }
+    }   
 }
 
-// var question = $('questions');
-// var answers = $('a');
-// var correctAns = $('correct');
-// var submit = $('submit');
-// var answers = $('answers');
-// var array = questions.length;
+displayQuestions()
 
-// var newQuestion = $("<div>");
-// for (var i = 0; i < questions.length; ++i) {
-//     questions = this.newQuestion[i];
-//     $('question').append('newQuestion')
+// for (var i = 0; i < questions.length; i++) {
+//     var subButton = $("<button>");
+//     subButton = addClass("submit-"
+//     )
+//     button.attr("data-answers", questions[i]);
+//     $("#optionA").append(button);
 // }
+
+$(".btn").on("click", function() {
+    $("#display").hide();
+    for (var i = 0; i < questions.length; i++) {
+        var radioValue = $("input[name='" + i + "']:checked").val();
+        if (radioValue === questions[i].correct) {
+            correctCounter++;
+        } else {
+            incorrectCounter++;
+        }
+    }
+    alert("correct guesses" + correctCounter);
+    alert("incorrect guesses" + incorrectCounter);
+})
+
+
+// if (q1 == "4") {
+//     correct++;
+// }
+// if (q2 == "3") {
+//     correct++;
+// }
+// if (q3 == "4") {
+//     correct++;
+// }
+// if (q4 == "4") {
+//     correct++;
+// }
+});
+
+//1. to compare userInputs, create a variable to hold the userInputValue that is checked
+//2. loop through the input variable
+//3. if statement inside the input loop
+
+//1. create results div
+//2. append results to created results div
+
